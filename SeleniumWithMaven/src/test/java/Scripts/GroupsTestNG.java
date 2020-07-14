22033027c1155eeb70d86fc45d7a740d22b0fefc
shsebastian <shaji.sebastian@ibsplc.com>
2020-07-14 16:01:02 +0530

    updated

--- SeleniumWithMaven/src/test/java/Scripts/GroupsTestNG.java
+++ SeleniumWithMaven/src/test/java/Scripts/GroupsTestNG.java
@@ -8,22 +8,22 @@ public class GroupsTestNG {
 	
 	@Test(groups = "Sanity")
 	public void test1() {
-		System.out.println("test1");
+		System.out.println("test 1");
 	}
 	
 	@Test(groups = {"Sanity","Smoke"})
 	public void test2() {
-		System.out.println("test2");
+		System.out.println("test 2");
 	}
 	
 	@Test(groups = "Regression")
 	public void test3() {
-		System.out.println("test3");
+		System.out.println("test 3");
 	}
 	
 	@Test(groups = "Regression")
 	public void test4() {
-		System.out.println("test4");
+		System.out.println("test 4");
 	}
 
 }
